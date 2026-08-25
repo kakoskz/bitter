5b4ea56806a995e2a23c8d4ffa32a0a9ca287d3c
kakoskz <francoescares5@gmail.com>
2026-08-25 00:22:56 -0400

    Tareas

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <script>
 import Card from './components/card.vue';
 import Form from './components/form.vue'
+import Tareas from './components/tareas.vue';
 export default {
 
   name: 'app',
-  components: { Card, Form }
+  components: { Card, Form , Tareas }
 }
 console.log('EL Hijo Me Aviso Algo')
 </script>
@@ -15,5 +16,10 @@ console.log('EL Hijo Me Aviso Algo')
   <div >
     <h2>Actividad 4</h2>
     <Form/>
+  </div>
+
+  <div>
+    <h2>Actividad 5</h2>
+    <Tareas/>
   </div>
 </template>
---
+++ src/components/tareas.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  name: 'tareas',
+  data() {
+    return {
+      nuevaTarea: '',
+      tareas: [],
+      mostrar: true
+    }
+  },
+  methods: {
+    agregarTarea() {
+      if (this.nuevaTarea.trim()) {
+        this.tareas.push(this.nuevaTarea)
+        this.nuevaTarea = ''
+      }
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="nuevaTarea" placeholder="Escribe una nueva tarea" />
+    <button @click="agregarTarea">Agregar</button>
+    <button @click="mostrar = !mostrar">
+      {{ mostrar ? 'Ocultar' : 'Mostrar' }} tareas
+    </button>
+
+    <ul v-show="mostrar">
+      <li v-for="(t, index) in tareas" :key="index">{{ t }}</li>
+    </ul>
+
+    <p v-if="tareas.length === 0">No hay tareas registradas</p>
+  </div>
+</template>
